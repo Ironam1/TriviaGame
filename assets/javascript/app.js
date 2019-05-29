2fd6630f788cee47361f8f0d501e24c0b4d97ce9
Ironam1 <ironam1@icloud.com>
2019-05-29 17:13:57 -0400

    need to fix random generator. pics won't load. game plays and resets properly

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,48 +2,57 @@ var questions = [{
     question: "What was the first game released by Nintendo?",
     answer: ["Mario Bros.", "Mr. Game & Watch", "Playing cards called, hanafuda", "I don't play games!"],
     check: 2,
-    correctAnswer: "The correct answer is, Hanafuda!"},
+    correctAnswer: "The correct answer is, Hanafuda!",
+    img: "assests/images/hanafuda.jpeg"},
     
     {question:"What is Nintendo's best selling game?",
     answer:["Super Mario Bros. 3", "Pokemon Red/Green/Blue/Yellow", "Who plays Nintendo anymore?", "Wii Sports"],
     check: 3,
-    correctAnswer:"The correct answer is, Wii Sports!"},
+    correctAnswer:"The correct answer is, Wii Sports!",
+    img: "assests/images/wii-fit.jpeg"},
     
     {question:"Which Nintendo series has become their best selling?",
     answer:["Metroid", "The Legend of Zelda", "Super Mario Bros.", "Pokemon"],
     check: 2,
-    correctAnswer:"The correct answer is, Super Mario Bros.!"},
+    correctAnswer:"The correct answer is, Super Mario Bros.!",
+    img: "assests/images/mario.jpeg"},
     
     {question:"Which battle royale game has become one the best selling fighter games?",
     answer:["Super Smash Bros.", "Tekken", "I don't even know how to fight!", "Mortal Combat"],
     check: 0,
-    correctAnswer:"The correct answer is, Super Smash Bros!"},
+    correctAnswer:"The correct answer is, Super Smash Bros!",
+    img: "assests/images/smash.jpeg"},
     
     {question:"What was Luigi's first solo game?",
     answer:["Luigi's Haunted Mansion", "We can't all be Mario!", "Smash Bros.", "The Lonley Plumber"],
     check: 0,
-    correctAnswer:"The correct answer is, Luigi's Haunted Mansion"},
+    correctAnswer:"The correct answer is, Luigi's Haunted Mansion",
+    img: "assests/images/luigi.jpeg"},
     
     {question:"What does NES stand for?",
     answer:["Nintendo Electronic System", "Nintendo Entertainment System", "Not Enough Sleep", "Nintendo Entertainment Station"],
     check: 1,
-    correctAnswer:"The correct answer is, Nintendo Entertainment System"},
+    correctAnswer:"The correct answer is, Nintendo Entertainment System",
+    img: "assests/images/NES.jpeg"},
     
     {question:"What year did the NES release in the United States?",
     answer:["1978", "1980", "1982", "1985"],
     check: 3,
-    correctAnswer:"The correct answer is, 1985"},
+    correctAnswer:"The correct answer is, 1985",
+    img: "assests/images/1985.png"},
     
 
     {question:"What is the official Nintendo magizine called?",
     answer:["Nintendo Monthly", "Print is dead!", "Nintendo Power", "Nintendo Game and Review"],
     check: 2,
-    correctAnswer:"The correct answer is, Nintendo Power"},
+    correctAnswer:"The correct answer is, Nintendo Power",
+    img: "assests/images/power.jpeg"},
     
     {question:"What was Nintendo's first handheld device called?",
     answer:["Nintendo Portable", "Game Boy", "Play-and-Go", "Vita"],
     check: 1,
-    correctAnswer:"The correct answer is, Game Boy"},
+    correctAnswer:"The correct answer is, Game Boy",
+    img: "assests/images/game-boy.jpeg"},
     
 ]
 
@@ -104,20 +113,30 @@ function stop () {
     $("#correct").show();
     $("#time-over").html(wrongAnswer1);
     $("#correct").html(shown.correctAnswer);
+    $("#pic").html("<img src=" + shown.img + ">");
     console.log("SHOWN " + shown)
     endGame();   
 }
 //create question array
 //create function to generate random question and display on page
 function getQuestion() {
-    
     $("#question").empty();    
     $("#guesses").empty();
+    $("#time-over").empty();
     $("#question").show();
     $("#timer").show();
     $("#guesses").show();
     
     next = Math.floor(Math.random()*questions.length);
+    questArray.push(next);
+    var playing = false;
+    console.log("NUMBER" + questArray);
+    for (var j = 0; j < questArray.length; j++) {
+    if (questArray[j] === next) {
+       playing = false;
+    
+    } 
+    if (!playing) {
     shown = questions[next];
     console.log(shown + "SHOWN");
     $("#question").html("<h3>" + shown.question + "</h3>");
@@ -129,6 +148,10 @@ function getQuestion() {
         choice.attr("pick", i);
         $("#guesses").append(choice);
     }
+} else {
+    endGame();
+}
+}
     makeGuess();
     // $("container").css("display", "block");
     // $("#question").html(shown.question);
@@ -172,7 +195,7 @@ function makeGuess (guess) {
 
 function endGame () {
     clearInterval(timer);
-    questArray.push(shown);
+    // questArray.push(shown);
     holder.splice(1, 1, shown);
     
     setTimeout (function() {
@@ -182,6 +205,7 @@ function endGame () {
         $("#final").append("<p>Correct: " + right + "</p>");
         $("#final").append("<p>Wrong: " + wrong + "</p");
         $("#final").append("<p>Over Limit: " + long + "</p");
+        resetGame();
         console.log(questions + "END ARRAY");
         console.log("HOLDER ARRAY" + questArray);
         } else {
@@ -191,8 +215,12 @@ function endGame () {
     }, 3000);
 }
  
-$(document).ready(function() {
+function resetGame () {
 $("#reset").on("click", function () {
+    right = 0;
+    wrong = 0;
+    long = 0;
+    questArray = [];
      $("#reset").hide();
      $("#final").hide();
      for (var i = 0; i < questions.length; i++) {
@@ -201,4 +229,4 @@ $("#reset").on("click", function () {
     startTimer();
     getQuestion();
     })
- })
+ }
